71d471e9d16bf9d5b9b0b0aed11a73534bbb3d69
lebon8 <drnlebonenian@gmail.com>
2026-06-29 22:05:04 +0200

    Add 1 to 6 for week 2 exercises

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,8 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+
+bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress "$ADDR" 1
+
+echo "$ADDR"
