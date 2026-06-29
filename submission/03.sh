c9d3da32c45963397b776888d1d9e82eec6fe8ea
lebon8 <drnlebonenian@gmail.com>
2026-06-29 22:41:37 +0200

    Add 1 to 8 for week 2 exercises

--- submission/03.sh
+++ submission/03.sh
@@ -3,6 +3,6 @@
 # Return only the Address
 ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 
-bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 101 $ADDR 
+addfunds=$(bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 102 $ADDR)
 
 bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $ADDR | jq -r '.address'
